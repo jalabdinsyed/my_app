1aca8e13ce00c4f8e9598a5ebfdec8fb686671eb
jalabdinsyed <jalabdin2022@gmail.com>
2025-12-30 17:31:04 +0530

    model gserialp

--- apps/business/serializers.py
+++ apps/business/serializers.py
@@ -1,53 +1,28 @@
-# from rest_framework import serializers
-# from .models import Business
-
-# class BusinessSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Business
-#         geo_field = "location"   # Important
-#         fields = (
-#             "id",
-#             "name",
-#             "category",
-#             "sub_category",
-#             "tags",
-#             "location",
-#             "created_at",
-#         )
-
-from rest_framework_gis.serializers import GeoFeatureModelSerializer
-from .models import Business
 from rest_framework import serializers
+from django.contrib.gis.geos import Point
+from .models import Business
 
-class BusinessSerializer(GeoFeatureModelSerializer):
+class BusinessSerializer(serializers.ModelSerializer):
+    lat = serializers.FloatField(write_only=True)
+    lng = serializers.FloatField(write_only=True)
+
     class Meta:
         model = Business
-        geo_field = "location"   # VERY IMPORTANT
         fields = (
             "id",
             "name",
             "category",
             "sub_category",
             "tags",
+            "lat",
+            "lng",
             "location",
             "created_at",
         )
+        read_only_fields = ("location", "created_at")
+
     def create(self, validated_data):
-        # 🔴 IMPORTANT: POP location FIRST
-        location_data = validated_data.pop("location", None)
-
-        # Create instance WITHOUT location
-        instance = Business.objects.create(**validated_data)
-
-        # Now safely set PointField
-        if location_data:
-            try:
-                lng, lat = location_data["coordinates"]
-                instance.location = Point(float(lng), float(lat))
-                instance.save()
-            except Exception:
-                raise serializers.ValidationError(
-                    {"location": "Invalid GeoJSON Point format"}
-                )
-
-        return instance
+        lat = validated_data.pop("lat")
+        lng = validated_data.pop("lng")
+        validated_data["location"] = Point(lng, lat)
+        return super().create(validated_data)
