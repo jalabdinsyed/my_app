1bd436d26e16f8fcf42f4af58fb30262d9f19eed
jalabdinsyed <jalabdin2022@gmail.com>
2025-12-30 17:21:04 +0530

    model gserial

--- apps/business/serializers.py
+++ apps/business/serializers.py
@@ -1,10 +1,27 @@
-from rest_framework import serializers
+# from rest_framework import serializers
+# from .models import Business
+
+# class BusinessSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Business
+#         geo_field = "location"   # Important
+#         fields = (
+#             "id",
+#             "name",
+#             "category",
+#             "sub_category",
+#             "tags",
+#             "location",
+#             "created_at",
+#         )
+
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from .models import Business
 
-class BusinessSerializer(serializers.ModelSerializer):
+class BusinessSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = Business
-        geo_field = "location"   # Important
+        geo_field = "location"   # VERY IMPORTANT
         fields = (
             "id",
             "name",
