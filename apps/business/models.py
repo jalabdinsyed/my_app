8d385d4bb6862656badfdacfdb9b8cf230443710
jalabdinsyed <jalabdin2022@gmail.com>
2025-12-30 16:18:30 +0530

    model geoiffi

--- apps/business/models.py
+++ apps/business/models.py
@@ -1,20 +1,10 @@
-# from django.db import models
-
-# class Business(models.Model):
-#     name = models.CharField(max_length=255)
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.name
-    
-
-from django.contrib.gis.db import models as gis_models
 from django.db import models
+from django.contrib.gis.db import models as gis_models
+from django.utils import timezone
 import uuid
 
 
 class Business(models.Model):
-    # business_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=255, db_index=True)
 
     category = models.CharField(max_length=100, db_index=True)
@@ -22,19 +12,10 @@ class Business(models.Model):
 
     tags = models.JSONField(default=list, blank=True)
 
-    # lat + long
-    location = gis_models.PointField(geography=True)
+    # GIS field ONLY
+    location = gis_models.PointField(null=True, blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
-
-# class Business(models.Model):
-#     name = models.CharField(max_length=255, null=True, blank=True)
-
-#     category = models.CharField(max_length=100, null=True, blank=True)
-#     sub_category = models.CharField(max_length=100)
-
-#     def __str__(self):
-#         return self.name
